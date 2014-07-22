e40233621bcfe1ab2432e7527040413aa5a1bfac
Sergey Dzyuban <sergey.dzyuban@gmail.com>
2014-07-22 18:20:32 +0300

    social libraries

---
+++ web.api/Web.API.Samples/Social/Web.API.Samples.Social.Twitter/Class1.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="Class1.cs" company="localhost">
+// Copyright (c) 2014 All Rights Reserved
+// </copyright>
+// <author>Sergey Dzyuban</author>
+// <date>6/18/2014 4:06:58 PM</date>
+// <summary></summary>
+
+namespace Web.API.Samples.Social.Twitter
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    /// <summary>
+    /// test 1 2 
+    /// </summary>
+    public class Class1
+    {
+    }
+}
---
+++ web.api/Web.API.Samples/Web.API.Samples.Host/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.API.Samples.Host.Models;
+
+namespace Web.API.Samples.Host.Controllers
+{
+    public class ProductController : AsyncController
+    {
+        //
+        // GET: /Product/
+
+        public ActionResult Index()
+        {
+            Product data = new Product();
+            data.Id = 1;
+            data.Name = "Test";
+            data.Notes = "Description";
+            return View(data);
+        }
+
+        public ActionResult Detail()
+        {
+            Product data = new Product();
+            data.Id = 1;
+            data.Name = "Test";
+            data.Notes = "Description";
+            return View(data);
+        }
+
+    }
+}
