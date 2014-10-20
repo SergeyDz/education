377a9dd4f8e868d49c47806f77f0eacdf79bf17f
SergeyDz <sergey.dzyuban@gmail.com>
2014-10-20 20:28:54 +0300

    HandleCollection example

---
+++ CLR/SD.CLR.Test/SafeHandle/Lessons/Lesson02.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="Lesson02.cs" company="localhost">
+// Copyright (c) 2014 All Rights Reserved
+// </copyright>
+// <author>Sergey Dzyuban</author>
+// <date>10/20/2014 8:07:22 PM</date>
+// <summary></summary>
+namespace SafeHandle.Lessons
+{
+    using CLR.Common.Contracts;
+    using SafeHandle.DomainModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Runtime.InteropServices;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Lesson02 : ICommand
+    {
+        public void Execute()
+        {
+            HandleCollector collector = new HandleCollector("Hello world collector", 4, 8);
+            for (int i = 0; i < 20; i++)
+            {
+                FileWriter writer = new FileWriter("Lesson02 - " + i + ".txt", collector);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+}
--- CLR/SD.CLR.Test/SafeHandle/Program.cs
+++ CLR/SD.CLR.Test/SafeHandle/Program.cs
@@ -1,4 +1,5 @@
-﻿using SafeHandle.DomainModel;
+﻿using CLR.Common.Contracts;
+using SafeHandle.DomainModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,19 +12,8 @@ namespace SafeHandle
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i < 10; i++)
-            {
-                using (var example = new DisposableExample("disposed." + i + ".txt"))
-                {
-                    Console.WriteLine("Run some code for i=" + i);
-                }
-              
-                var example2 = new DisposableExample("undisposed." + i + ".txt");
-            }
-
-            Console.WriteLine("GC collecting...");
-            GC.Collect();
-
+            ICommand command = new Lessons.Lesson02();
+            command.Execute();
             Console.ReadKey();
         }
     }
